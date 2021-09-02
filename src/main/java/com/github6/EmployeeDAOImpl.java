219b4642b406dc1dc003634bac35815347adb943
Rohit <Rohit@10.0.0.214>
2021-09-01 23:29:53 -0400

    Please approve

---
+++ src/main/java/com/github6/EmployeeDAOImpl.java
@@ -0,0 +1,28 @@
+package com.github6;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+	public void saveEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateEmployee(int empId, int empSalary) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteEmployee(int empId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Employee> listEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
